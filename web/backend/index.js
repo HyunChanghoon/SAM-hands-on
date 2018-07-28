760b849c99049b151204465aaf2ba4dee18a9346
HyunChanghoon <chhyun891119@gmail.com>
2018-07-28 06:32:32 +0000

    add msg

--- web/backend/index.js
+++ web/backend/index.js
@@ -39,6 +39,7 @@ exports.handler = (event, context, callback) => {
             TableName: process.env.tableName
         };
         docClient.scan(params, (err, data) => {
+            data.msg = 'hello SAM!';
             const result = err
                 ? [err]
                 : [err, {
